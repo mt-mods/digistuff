773364907b2927c5a3d8a17243a74758288cf4ad
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-12-25 09:53:14 +0100

    minetest -> luanti/core
    helps a little with line lengths

--- controller.lua
+++ controller.lua
@@ -21,7 +21,7 @@ local last_seen_inputs = {}
 --       entities. Maybe we better handle those instead of using this
 --       somewhat expensive search every time some player detaches.
 local function removeEntity(pos)
-	local entitiesNearby = minetest.get_objects_inside_radius(pos,0.5)
+	local entitiesNearby = core.get_objects_inside_radius(pos,0.5)
 	for _,i in pairs(entitiesNearby) do
 		if i:get_luaentity() and i:get_luaentity().name == "digistuff:controller_entity" then
 			i:remove()
@@ -30,15 +30,15 @@ local function removeEntity(pos)
 end
 
 local function process_inputs(pos)
-	local meta = minetest.get_meta(pos)
-	local hash = minetest.hash_node_position(pos)
+	local meta = core.get_meta(pos)
+	local hash = core.hash_node_position(pos)
 	local name = players_on_controller[hash]
-	local player = minetest.get_player_by_name(name)
-	if minetest.get_node(pos).name ~= "digistuff:controller_programmed" then
+	local player = core.get_player_by_name(name)
+	if core.get_node(pos).name ~= "digistuff:controller_programmed" then
 		if player then
 			player:set_physics_override({speed = 1,jump = 1,})
 			player:set_pos(vector.add(pos,vector.new(0.25,0,0.25)))
-			minetest.chat_send_player(name, "You are now free to move.")
+			core.chat_send_player(name, "You are now free to move.")
 		end
 		last_seen_inputs[name] = nil
 		players_on_controller[hash] = nil
@@ -47,7 +47,7 @@ local function process_inputs(pos)
 
 	if not player then
 		digilines.receptor_send(pos,digiline_rules,meta:get_string("channel"),"player_left")
-		minetest.get_meta(pos):set_string("infotext","Digilines Game Controller Ready\n(right-click to use)")
+		core.get_meta(pos):set_string("infotext","Digilines Game Controller Ready\n(right-click to use)")
 		players_on_controller[hash] = nil
 		return
 	end
@@ -77,9 +77,9 @@ local function process_inputs(pos)
 end
 
 local function release_player(pos)
-	local hash = minetest.hash_node_position(pos)
+	local hash = core.hash_node_position(pos)
 	local name = players_on_controller[hash]
-	local player = minetest.get_player_by_name(name)
+	local player = core.get_player_by_name(name)
 	if player then
 		local parent = player:get_attach()
 		local lua_entity = parent and parent:get_luaentity()
@@ -87,11 +87,11 @@ local function release_player(pos)
 			-- Remove also detaches
 			parent:remove()
 		end
-		minetest.chat_send_player(name, "You are now free to move.")
+		core.chat_send_player(name, "You are now free to move.")
 	end
 	-- Shouldn't find any more entities now that above code is fixed
 	removeEntity(pos)
-	local meta = minetest.get_meta(pos)
+	local meta = core.get_meta(pos)
 	meta:set_string("infotext","Digilines Game Controller Ready\n(right-click to use)")
 	last_seen_inputs[name] = nil
 	players_on_controller[hash] = nil
@@ -99,33 +99,33 @@ local function release_player(pos)
 end
 
 local function trap_player(pos,player)
-	local hash = minetest.hash_node_position(pos)
+	local hash = core.hash_node_position(pos)
 	local oldname = players_on_controller[hash]
 	local newname = player:get_player_name()
-	if oldname and minetest.get_player_by_name(oldname) then
-			minetest.chat_send_player(newname,
+	if oldname and core.get_player_by_name(oldname) then
+			core.chat_send_player(newname,
 				"Controller is already occupied by " .. oldname)
 			return
 	else
 		players_on_controller[hash] = newname
-		local entity = minetest.add_entity(pos,"digistuff:controller_entity")
+		local entity = core.add_entity(pos,"digistuff:controller_entity")
 		player:set_attach(entity,"",vector.new(0,0,0),vector.new(0,0,0))
-		minetest.chat_send_player(newname,"You are now using a digilines game controller. Right-click the controller again to be released.")
-		local meta = minetest.get_meta(pos)
+		core.chat_send_player(newname,"You are now using a digilines game controller. Right-click the controller again to be released.")
+		local meta = core.get_meta(pos)
 		meta:set_string("infotext","Digilines Game Controller\nIn use by: "..newname)
 		process_inputs(pos)
 	end
 end
 
 local function toggle_trap_player(pos,player)
-	if players_on_controller[minetest.hash_node_position(pos)] then
+	if players_on_controller[core.hash_node_position(pos)] then
 		release_player(pos)
 	else
 		trap_player(pos,player)
 	end
 end
 
-minetest.register_node("digistuff:controller", {
+core.register_node("digistuff:controller", {
 	description = "Digilines Game Controller",
 	tiles = {
 		"digistuff_controller_top.png",
@@ -141,29 +141,29 @@ minetest.register_node("digistuff:controller", {
 	},
 	_digistuff_channelcopier_fieldname = "channel",
 	_digistuff_channelcopier_onset = function(pos)
-		local meta = minetest.get_meta(pos)
+		local meta = core.get_meta(pos)
 		meta:set_string("formspec","")
 		meta:set_string("infotext","Digilines Game Controller Ready\n(right-click to use)")
-		minetest.swap_node(pos,{name = "digistuff:controller_programmed",})
+		core.swap_node(pos,{name = "digistuff:controller_programmed",})
 	end,
 	groups = {cracky = 1,},
 	is_ground_content = false,
 	on_construct = function(pos)
-		local meta = minetest.get_meta(pos)
+		local meta = core.get_meta(pos)
 		meta:set_string("formspec","field[channel;Channel;${channel}")
 	end,
 	on_receive_fields = function(pos, formname, fields, sender)
 		local name = sender:get_player_name()
-		if minetest.is_protected(pos,name) and not minetest.check_player_privs(name,{protection_bypass=true}) then
-			minetest.record_protection_violation(pos,name)
+		if core.is_protected(pos,name) and not core.check_player_privs(name,{protection_bypass=true}) then
+			core.record_protection_violation(pos,name)
 			return
 		end
-		local meta = minetest.get_meta(pos)
+		local meta = core.get_meta(pos)
 		if fields.channel then
 			meta:set_string("channel",fields.channel)
 			meta:set_string("formspec","")
 			meta:set_string("infotext","Digilines Game Controller Ready\n(right-click to use)")
-			minetest.swap_node(pos,{name = "digistuff:controller_programmed",})
+			core.swap_node(pos,{name = "digistuff:controller_programmed",})
 		end
 	end,
 	digiline = {
@@ -174,7 +174,7 @@ minetest.register_node("digistuff:controller", {
 	},
 })
 
-minetest.register_node("digistuff:controller_programmed", {
+core.register_node("digistuff:controller_programmed", {
 	description = "Digilines Game Controller (programmed state - you hacker you!)",
 	drop = "digistuff:controller",
 	tiles = {
@@ -234,10 +234,10 @@ minetest.register_node("digistuff:controller_programmed", {
 		},
 		effector = {
 			action = function(pos,node,channel,msg)
-				local setchannel = minetest.get_meta(pos):get_string("channel")
+				local setchannel = core.get_meta(pos):get_string("channel")
 				if channel ~= setchannel then return end
 				if msg == "release" then
-					local hash = minetest.hash_node_position(pos)
+					local hash = core.hash_node_position(pos)
 					if players_on_controller[hash] then
 						release_player(pos)
 					end
@@ -247,7 +247,7 @@ minetest.register_node("digistuff:controller_programmed", {
 	},
 })
 
-minetest.register_entity("digistuff:controller_entity",{
+core.register_entity("digistuff:controller_entity",{
 	initial_properties = {
 		visual = "sprite",
 		physical = false,
@@ -259,31 +259,31 @@ minetest.register_entity("digistuff:controller_entity",{
 
 local acc_dtime = 0
 
-minetest.register_globalstep(function(dtime)
+core.register_globalstep(function(dtime)
 	acc_dtime = acc_dtime + dtime
 	if acc_dtime < 0.2 then return end
 	acc_dtime = 0
 	for hash in pairs(players_on_controller) do
-		local pos = minetest.get_position_from_hash(hash)
+		local pos = core.get_position_from_hash(hash)
 		process_inputs(pos)
 	end
 end)
 
-minetest.register_lbm({
+core.register_lbm({
 	name = "digistuff:reset_controllers",
 	label = "Reset game controllers to idle",
 	nodenames = {"digistuff:controller_programmed"},
 	run_at_every_load = true,
 	action = function(pos)
-		if not players_on_controller[minetest.hash_node_position(pos)] then
-			local meta = minetest.get_meta(pos)
+		if not players_on_controller[core.hash_node_position(pos)] then
+			local meta = core.get_meta(pos)
 			digilines.receptor_send(pos,digiline_rules,meta:get_string("channel"),"player_left")
 			meta:set_string("infotext","Digilines Game Controller Ready\n(right-click to use)")
 		end
 	end,
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "digistuff:controller",
 	recipe = {
 		{"","digistuff:button","",},
